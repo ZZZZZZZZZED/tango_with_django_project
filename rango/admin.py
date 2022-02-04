611ba8463bdf14926e18a3555327723a71baf4bc
ZZZZZZZZZED <2613043L@student.gla.ac.uk>
2022-02-04 11:48:38 +0000

    Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
+from sre_constants import CATEGORY_DIGIT
+from tkinter import PAGES
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+admin.site.register(Category)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
