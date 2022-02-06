2190b71d239eb1ecdfceb4f76080f6d44167fecb
ZZZZZZZZZED <2613043L@student.gla.ac.uk>
2022-02-06 18:42:18 +0000

    Chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -3,8 +3,10 @@ from tkinter import PAGES
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 
 
 class PageAdmin(admin.ModelAdmin):
